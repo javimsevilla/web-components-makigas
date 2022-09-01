52a9ed05225d66747ed3d75d199a785197c50407
Javi Molina <jmolina@avantio.com>
2022-09-01 19:31:32 +0200

    add custom element text
    ...using `connectedCallback` method, that executes when element is
    added to the DOM.

--- components.js
+++ components.js
@@ -7,6 +7,15 @@ class BasicGreetingElement extends HTMLElement {
     super();
   }
 
+  /**
+   * Executes when element is added to the DOM
+   */
+  connectedCallback() {
+    let title = document.createElement('h1');
+    title.innerHTML = 'Hello, World!';
+    this.append(title);
+  }
+
 }
 
 window.customElements.define('basic-greeting', BasicGreetingElement);
